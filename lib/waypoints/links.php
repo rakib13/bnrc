81550abfe56304664bd3cc541199207a48b6b3b6
rakib13 <rakib.omar13@gmail.com>
2025-05-13 15:35:20 +0600

    code merge by Rakib

--- lib/waypoints/links.php
+++ lib/waypoints/links.php
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 <?php
   $links = array(
     'js' => 'lib/waypoints/waypoints.min.js'
   );
 ?>
-=======
-<?php
-  $links = array(
-    'js' => 'lib/waypoints/waypoints.min.js'
-  );
-?>
->>>>>>> d779bfb355e8d1bd03863054101c286a45a8fe66
